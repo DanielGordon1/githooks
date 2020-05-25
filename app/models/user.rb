59b4f8bf08fa9341abe761db8ab9f87b00b5fb6d
daniel <d_gordon1@icloud.com>
2020-05-25 13:43:50 +0200

    FEAT: add association between user and release.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :external_id }
   validates :email, presence: true, uniqueness: { scope: :external_id }
   validates :external_id, presence: true, uniqueness: true
+
+  has_many :releases
 end
