f55a1f1d73653165d49679a0bcc9cba9094f549f
daniel <d_gordon1@icloud.com>
2020-05-25 12:47:50 +0200

    FIX: assumption about generator.

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module Githooks
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.test_framework :rspec,
+    config.generators.test_framework :rspec,
       fixtures: false,
       view_specs: false,
       helper_specs: false,
