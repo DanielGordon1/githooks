db5f66b4007ce781a5f756b10c99d7d9876e238e
daniel <d_gordon1@icloud.com>
2020-05-25 13:28:18 +0200

    FEAT: add first user spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,11 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "valid records" do
-
+  describe 'valid records' do
+    it 'had a valid factory' do
+      expect(build(:user)).to be_valid
+    end
   end
 end
