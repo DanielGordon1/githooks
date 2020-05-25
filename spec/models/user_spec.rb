2aa00804a36ef8191ee6298f25eeef9b031c6651
daniel <d_gordon1@icloud.com>
2020-05-25 13:34:30 +0200

    FEAT: add specs for user model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe User, type: :model do
     it 'had a valid factory' do
       expect(build(:user)).to be_valid
     end
+
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:external_id) }
+
+    it { should validate_uniqueness_of(:name).scoped_to(:external_id) }
+    it { should validate_uniqueness_of(:email).scoped_to(:external_id) }
   end
 end
