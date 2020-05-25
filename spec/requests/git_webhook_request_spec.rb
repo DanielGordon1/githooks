a43f404cd8ed14f62b8b8fb5649a49fc50c3a443
daniel <d_gordon1@icloud.com>
2020-05-25 16:41:11 +0200

    FEAT: setup basic start of request spec

--- spec/requests/git_webhook_request_spec.rb
+++ spec/requests/git_webhook_request_spec.rb
@@ -3,9 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe 'GitWebhooks', type: :request do
+  let(:push_payload) { file_fixture('push.json').read }
+  let(:pull_request_payload) { file_fixture('pull_request.json').read }
+  let(:release) { file_fixture('release.json').read }
+
   describe 'POST#create' do
     it 'returns http success' do
-      post '/githook'
+      headers = { "CONTENT-TYPE" => "application/json" }
+      post '/v1/githook', params: push_payload, headers: headers
 
       expect(response).to have_http_status(:success)
     end
