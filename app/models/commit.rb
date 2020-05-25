8d52bce10a4c20e3e4c0132f6fbe51d96ed978c1
daniel <d_gordon1@icloud.com>
2020-05-25 14:52:50 +0200

    FEAT: add basic specs for commit model

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,5 +1,7 @@
 class Commit < ApplicationRecord
+  validates :message, :sha, :committed_at, :ticket_identifiers, presence: true
+  validates :sha, uniqueness: true
+
   belongs_to :user
   belongs_to :release
-  belongs_to :user
 end
--- spec/models/commit_spec.rb
+++ spec/models/commit_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Commit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'valid records' do
+    it 'has a valid factory' do
+      expect(build(:release)).to be_valid
+    end
+
+    it { should validate_presence_of(:message) }
+    it { should validate_presence_of(:committed_at) }
+    it { should validate_presence_of(:sha) }
+
+    it { should belong_to(:user) }
+    it { should belong_to(:release) }
+  end
 end
