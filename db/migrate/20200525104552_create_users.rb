80ab1606c2fd6428a5ac371d34f234976668e247
daniel <d_gordon1@icloud.com>
2020-05-25 13:12:32 +0200

    FEAT: adjust user migration to incorporate correct type.

--- db/migrate/20200525104552_create_users.rb
+++ db/migrate/20200525104552_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :external_id
+      t.integer :external_id
 
       t.timestamps
     end
