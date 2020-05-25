d12e08dbfc0c638b176841ec87710f56bbcea1d2
daniel <d_gordon1@icloud.com>
2020-05-25 12:51:28 +0200

    FEAT: add validations to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: { scope: :external_id }
+  validates :email, presence: true, uniqueness: { scope: :external_id }
+  validates :external_id, presence: true, uniqueness: true
 end
