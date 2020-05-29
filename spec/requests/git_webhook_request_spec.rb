2abdf1dfac6b3df230528f05a04523ab067adb9e
daniel <d_gordon1@icloud.com>
2020-05-29 15:02:00 +0200

    FEAT: add spec for unauthorized.

--- spec/requests/git_webhook_request_spec.rb
+++ spec/requests/git_webhook_request_spec.rb
@@ -13,13 +13,24 @@ RSpec.describe 'GitWebhooks', type: :request do
   end
 
   describe 'POST#create' do
-    it 'returns http success' do
-      expect(GithubWebhookStorageService).to receive(:new).and_return(service)
+    context 'with correct referrer' do
+      it 'returns http success' do
+        expect(GithubWebhookStorageService).to receive(:new).and_return(service)
 
-      headers = { 'CONTENT-TYPE' => 'application/json', 'HTTP_REFERER' => 'https://www.github.com' }
-      post '/v1/github', params: push_payload, headers: headers
+        headers = { 'CONTENT-TYPE' => 'application/json', 'HTTP_REFERER' => 'https://www.github.com' }
+        post '/v1/github', params: push_payload, headers: headers
 
-      expect(response).to have_http_status(:success)
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'with incorrect referrer' do
+      it 'returns http unauthorized' do
+        headers = { 'CONTENT-TYPE' => 'application/json', 'HTTP_REFERER' => 'https://www.some_other_hub.com' }
+        post '/v1/github', params: push_payload, headers: headers
+
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
   end
 end
