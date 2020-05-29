b118769ab7ad358737557f154813fa59d59ff88f
daniel <d_gordon1@icloud.com>
2020-05-29 14:57:46 +0200

    FEAT: setup a empty service spec.

---
+++ spec/services/github_webhook_storage_service.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe GithubWebhookStorageService do
+  describe '#call' do
+    context 'with webhook data relating to a PullRequest' do
+      it 'should store commit objects correctly' do
+
+      end
+    end
+
+    context 'with webhook data relating to a Push' do
+      it 'should store commit objects correctly' do
+
+      end
+    end
+
+    context 'with webhook data relating to a Release' do
+      it 'should store commit objects correctly' do
+
+      end
+    end
+  end
+end
