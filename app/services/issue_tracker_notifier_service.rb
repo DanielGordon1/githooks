9c9e86b36b8f32cc7069d2da3ad3d0d726ec6040
daniel <d_gordon1@icloud.com>
2020-05-31 18:59:30 +0200

    FEAT: req working for commits

--- app/services/issue_tracker_notifier_service.rb
+++ app/services/issue_tracker_notifier_service.rb
@@ -3,10 +3,22 @@
 # This service will notify a the issue tracking system by means of an HTTP req
 class IssueTrackerNotifierService
   def initialize(commits:, release: false)
-
+    @commits = commits
+    @release = release
+    @url = 'https://webhook.site/0d97a487-cd54-40e1-905a-052d344eb59a'
   end
 
   def call
+    if @release
 
+    else
+      @commits.each { |commit| send_notification(commit.notification_format) }
+    end
+  end
+
+  private
+
+  def send_notification(body)
+    HTTParty.post(@url, query: body)
   end
 end
--- spec/services/issue_tracker_notifier_service_spec.rb
+++ spec/services/issue_tracker_notifier_service_spec.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe IssueTrackerNotifierService do
-  desribe '#call' do
+
+  let(:service) { described_class }
+  describe '#call' do
     context 'with commits relating to a release' do
       it 'sends an HTTP req containing information about the release' do
       end
@@ -9,6 +11,8 @@ RSpec.describe IssueTrackerNotifierService do
 
     context 'with commits that do not belong to a release' do
       it 'sends an HTTP req containing information about the commit' do
+        service.new(commits: [build(:commit)]).call
+
       end
     end
   end
