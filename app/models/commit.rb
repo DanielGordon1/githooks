23f0fa9c4ee2bd010e625b92c47ad2846b5e29be
daniel <d_gordon1@icloud.com>
2020-05-31 18:51:34 +0200

    FEAT: add display methods to commit model

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -7,4 +7,16 @@ class Commit < ApplicationRecord
 
   belongs_to :author, class_name: 'User', foreign_key: :user_id
   belongs_to :release, optional: true
+
+  def notification_format
+    {
+      query: 'state - ready for release',
+      issues: unique_tickets.map { |id| { id: id } },
+      comment: "See SHA #{sha}"
+    }
+  end
+
+  def unique_tickets
+    ticket_identifiers.map { |k, v| v.map { |n| "#{k}-#{n}" } }.flatten
+  end
 end
--- spec/models/commit_spec.rb
+++ spec/models/commit_spec.rb
@@ -3,6 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe Commit, type: :model do
+  let(:commit) { build(:commit) }
+  let(:required_format) do
+    {
+      query: 'state - ready for release',
+      issues: [
+        { id: 'eve-214' },
+        { id: 'eve-211' },
+        { id: 'sp-421' }
+      ],
+      comment: 'See SHA 3cb53bc31415221cfb92358dd6f7f05d6f139fec'
+    }
+  end
+
   describe 'valid records' do
     it 'has a valid factory' do
       expect(build(:release)).to be_valid
@@ -16,5 +29,17 @@ RSpec.describe Commit, type: :model do
 
     it { should belong_to(:author) }
     it { should belong_to(:release).optional }
+  end
+
+  describe '#unique_tickets' do
+    it 'returns an array of strings in the correct format' do
+      expect(commit.unique_tickets).to eq(%w[eve-214 eve-211 sp-421])
+    end
+  end
+
+  describe '#notification_format' do
+    it 'returns data about the commit in the correct format' do
+      expect(commit.notification_format).to eq(required_format)
+    end
   end
 end
