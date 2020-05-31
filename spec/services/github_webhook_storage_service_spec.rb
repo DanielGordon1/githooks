3bb1a498cfc7a8e3787b661ad86fdf231acda029
daniel <d_gordon1@icloud.com>
2020-05-31 17:44:50 +0200

    CLEAN: enhance semantics of tests."

--- spec/services/github_webhook_storage_service_spec.rb
+++ spec/services/github_webhook_storage_service_spec.rb
@@ -13,13 +13,13 @@ RSpec.describe GithubWebhookStorageService, type: :service do
     context 'with webhook data relating to a PullRequest' do
       let(:pull_service) { service.new(webhook_data: pull_request_payload) }
 
-      it 'should select the correct handler' do
+      it 'selects the correct handler' do
         expect(pull_service).to receive(:select_handler).and_return('pull_request')
         expect(pull_service).to receive(:handle_pull_request)
         pull_service.call
       end
 
-      it 'should store commit objects correctly' do
+      it 'stores commit objects correctly' do
         expect(Commit.count).to eq(0)
         pull_service.call
         results = Commit.where("(ticket_identifiers->'sp') IS NOT NULL")
@@ -31,13 +31,13 @@ RSpec.describe GithubWebhookStorageService, type: :service do
     context 'with webhook data relating to a Push' do
       let(:push_service) { service.new(webhook_data: push_payload) }
 
-      it 'should select the correct handler' do
+      it 'selects the correct handler' do
         expect(push_service).to receive(:select_handler).and_return('push')
         expect(push_service).to receive(:handle_push)
         push_service.call
       end
 
-      it 'should store commit objects correctly' do
+      it 'stores commit objects correctly' do
         expect(Commit.count).to eq(0)
         push_service.call
         results = Commit.where("(ticket_identifiers->'sp') IS NOT NULL")
@@ -49,13 +49,13 @@ RSpec.describe GithubWebhookStorageService, type: :service do
     context 'with webhook data relating to a Release' do
       let(:release_service) { service.new(webhook_data: release_payload) }
 
-      it 'should select the correct handler' do
+      it 'selects the correct handler' do
         expect(release_service).to receive(:select_handler).and_return('release')
         expect(release_service).to receive(:handle_release)
         release_service.call
       end
 
-      it 'should store commit objects correctly' do
+      it 'stores commit objects correctly' do
         expect(Commit.count).to eq(0)
         release_service.call
         results = Commit.where("
