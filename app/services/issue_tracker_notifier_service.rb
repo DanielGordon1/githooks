432780e6ed9111fa5adb749e825f17f7408fc59d
daniel <d_gordon1@icloud.com>
2020-05-31 17:47:15 +0200

    FEAT: add basic service definition

---
+++ app/services/issue_tracker_notifier_service.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# This service will notify a the issue tracking system by means of an HTTP req
+class IssueTrackerNotifierService
+  def initialize
+
+  end
+
+  def call
+
+  end
+end
